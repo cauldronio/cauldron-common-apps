93c1044f1236b7525b0b8b9103d6c6b761572673
JJMerchante <jj.merchante@gmail.com>
2021-12-20 08:08:01 +0100

    [sbom] Include SBOM field to Report models
    Signed-off-by: JJMerchante <jj.merchante@gmail.com>

--- cauldron_apps/cauldron/models/project.py
+++ cauldron_apps/cauldron/models/project.py
@@ -23,6 +23,7 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     public = models.BooleanField(default=True)
     autorefresh = models.BooleanField(default=False)
+    sbom = models.BooleanField(default=False)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL,
                                 on_delete=models.SET_NULL,
                                 blank=True,
